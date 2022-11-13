d26f3cc15b5c42bb2b836a7d6b7a4a02ed222dee
Windu Risky Adiatma <windu.risky@bukalapak.com>
2022-11-13 15:14:32 +0700

    anawindu home pag

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -51,9 +51,5 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-  ],
+  plugins: [],
 };
