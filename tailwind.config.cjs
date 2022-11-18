bcddaa511f09fc4f3b71d1f8813349f0dcf64c89
Windu Risky Adiatma <windu.risky@bukalapak.com>
2022-11-19 00:35:50 +0700

    halaman waktu acara

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,6 +12,7 @@ module.exports = {
     colors: {
       transparent: 'transparent',
       current: 'currentColor',
+      black: '#000000',
       white: '#ffffff',
       'champagne-pink': {
         50: '#fffff3',
